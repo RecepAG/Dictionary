5a838fd46180204163f2aba4badf7fc54a3154d8
RecepAG <gokmenrecepali@gmail.com>
2021-01-25 21:58:07 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T1,T2>
+    {
+
+        T1[] studentName;
+        T2[] studentNumber;
+
+        public MyDictionary()
+        {
+            studentName = new T1[0];
+            studentNumber = new T2[0];
+        }
+
+        public void Add(T1 studentNames, T2 studentNumbers)
+        {
+            T1[] tempStudentName = studentName;
+            T2[] tempStudentNumber = studentNumber;
+
+            studentName = new T1[studentName.Length + 1];
+            studentNumber = new T2[studentNumber.Length + 1];
+
+            for (int i = 0; i < tempStudentName.Length; i++)
+            {
+                studentName[i] = tempStudentName[i];
+                studentNumber[i] = tempStudentNumber[i];
+            }
+
+            studentName[studentName.Length - 1] = studentNames;
+            studentNumber[studentNumber.Length - 1] = studentNumbers;
+
+            
+
+        }
+        public void List()
+        {
+            Console.WriteLine("List of Students");
+
+            for (int i = 0; i < studentName.Length; i++)
+            {
+                Console.WriteLine("Öğrenci No : " + studentNumber[i] + " Ad Soyad : "+studentName[i]);
+
+            }
+        }
+
+
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> student = new MyDictionary<string , int>();
+            student.Add("Mehmet Uslu" ,154);
+            student.Add("Ahmet Çalışkan",452);
+            student.Add("Hakan Ünsal" ,753);
+
+            student.List();
+
+            student.Add("Arda Kurt", 545);
+            student.List();
+
+            
+
+
+            
+
+
+        }
+    }
+}
